9ea278c7acda88c7dc291a69252d96bbb9dd2f0f
Liam Arzola <liam0215@gmail.com>
2025-09-03 09:04:33 -0700

    qat support in tests

--- ed25519-program/src/lib.rs
+++ ed25519-program/src/lib.rs
@@ -352,6 +352,24 @@ pub mod test {
 
     #[test]
     fn test_message_data_offsets() {
+        qat_shim::qat::start_session("SSL").expect("start session failed");
+        qat_shim::qat::qae_mem_init().expect("qae_mem_init failed");
+        let inst: Instance = qat::get_first_instance().expect("failed to get first instance");
+        inst.set_address_translation()
+            .expect("set address translation failed");
+        inst.start().expect("start instance failed");
+        let (tx_poll, poll) = if inst.is_polled().unwrap() {
+            let (tx, rx) = channel();
+            let inst2 = inst.clone();
+            let poll = std::thread::spawn(move || {
+                while matches!(rx.try_recv(), Err(TryRecvError::Empty)) {
+                    let _ = inst2.clone().poll_once();
+                }
+            });
+            (Some(tx), Some(poll))
+        } else {
+            (None, None)
+        };
         let offsets = Ed25519SignatureOffsets {
             message_data_offset: 99,
             message_data_size: 1,
@@ -391,6 +409,14 @@ pub mod test {
             test_case(1, &offsets),
             Err(PrecompileError::InvalidDataOffsets)
         );
+        if let Some(tx_poll) = tx_poll {
+            tx_poll
+                .send(())
+                .expect("Failed to send stop signal to polling thread");
+            poll.unwrap().join().expect("Polling thread panicked");
+        }
+        inst.stop().expect("stop instance failed");
+        qat_shim::qat::stop_session().expect("stop session failed");
     }
 
     #[test]
